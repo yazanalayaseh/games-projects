a8406be34399e8502c0dc3c4dc0c25a19fc27131
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-07-16 14:42:39 -0400

    Added the main function

--- scissor_paper_rock.js
+++ scissor_paper_rock.js
@@ -4,17 +4,21 @@ const computer = 0;
 
 
 
-const scissors_img=document.getElementById("sissors")
-scissors_img.addEventListener("click", function(){
-    console.log("clikcME");
-})
+function main(){
+    const scissors_img=document.getElementById("sissors")
+    scissors_img.addEventListener("click", function(){
+        console.log("clikcME");
+    })
 
-const rock_img=document.getElementById("rock")
-rock_img.addEventListener("click", function(){
-    console.log("clikcME");
-})
+    const rock_img=document.getElementById("rock")
+    rock_img.addEventListener("click", function(){
+        console.log("clikcME");
+    })
 
-const paper_img=document.getElementById("paper")
-paper_img.addEventListener("click", function(){
-    console.log("clikcME");
-})
+    const paper_img=document.getElementById("paper")
+    paper_img.addEventListener("click", function(){
+        console.log("clikcME");
+    })
+}
+
+main();
