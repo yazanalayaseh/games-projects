d11451fe6e00ace8601b04ca0948c54a73bc01d1
Yazanalayaseh <yalissa@gmail.com>
2020-07-16 23:47:31 -0400

    Changed style.css file

--- scissor_paper_rock.js
+++ scissor_paper_rock.js
@@ -1,51 +1,45 @@
 let userScore = 0;
 let computerScore = 0;
 
-let ComputerChoice = ()=> {
+let ComputerChoice = () => {
     let array = ['r', 'p', 's']
-    Math.round(Math.random() *2);
-    return array[Math.round(Math.random() *2)]
+    Math.round(Math.random() * 2);
+    return array[Math.round(Math.random() * 2)]
 }
 
 console.log(ComputerChoice())
 
 
 
-
-
-
-
-
-
-function game(UserChoice, ComputerChoice){
-console.log(ComputerChoice, UserChoice)
+function game(UserChoice, ComputerChoice) {
+    console.log(ComputerChoice, UserChoice)
 }
 
-function game(u,c){
-    if(UserChoice+ComputerChoice ==='rp' || UserChoice+ComputerChoice ==='sr' || UserChoice+ComputerChoice ==='ps'){
-    lose()
+function game(u, c) {
+    if (UserChoice + ComputerChoice === 'rp' || UserChoice + ComputerChoice === 'sr' || UserChoice + ComputerChoice === 'ps') {
+        lose()
     }
-     if(UserChoice+ComputerChoice ==='pr' || UserChoice+ComputerChoice ==='rs' || UserChoice+ComputerChoice ==='sp'){
-    win()
+    if (UserChoice + ComputerChoice === 'pr' || UserChoice + ComputerChoice === 'rs' || UserChoice + ComputerChoice === 'sp') {
+        win()
     }
-  }
-   if(UserChoice+ComputerChoice ==='pp' || UserChoice+ComputerChoice ==='ss' || UserChoice+ComputerChoice ==='rr'){
+}
+if (UserChoice + ComputerChoice === 'pp' || UserChoice + ComputerChoice === 'ss' || UserChoice + ComputerChoice === 'rr') {
     draw()
-    }
+}
   }
   }
 
 
-function main(){
-    document.getElementById("s").addEventListener("click", function(){
+function main() {
+    document.getElementById("s").addEventListener("click", function () {
         game("s", computerSelection());
     })
 
-    document.getElementById("r").addEventListener("click", function(){
+    document.getElementById("r").addEventListener("click", function () {
         game("r");
     })
 
-     document.getElementById("p").addEventListener("click", function(){
+    document.getElementById("p").addEventListener("click", function () {
         game("p");
     })
 }
