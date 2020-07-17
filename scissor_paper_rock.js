efe70834c3cb2912c6a2133c57b9f32c4dfb7b53
Yazanalayaseh <yalissa@gmail.com>
2020-07-16 23:05:51 -0400

    Added functions

--- scissor_paper_rock.js
+++ scissor_paper_rock.js
@@ -1,24 +1,59 @@
-const player = 0;
-const computer = 0;
+let userScore = 0;
+let computerScore = 0;
+
+let ComputerChoice = ()=> {
+    let array = ['r', 'p', 's']
+    Math.round(Math.random() *2);
+    return array[Math.round(Math.random() *2)]
+}
+
+console.log(ComputerChoice())
 
 
+
+
+
+
+
+
+
+function game(UserChoice, ComputerChoice){
+console.log(ComputerChoice, UserChoice)
+}
+
+function game(u,c){
+    if(UserChoice+ComputerChoice ==='rp' || UserChoice+ComputerChoice ==='sr' || UserChoice+ComputerChoice ==='ps'){
+    lose()
+    }
+     if(UserChoice+ComputerChoice ==='pr' || UserChoice+ComputerChoice ==='rs' || UserChoice+ComputerChoice ==='sp'){
+    win()
+    }
+  }
+   if(UserChoice+ComputerChoice ==='pp' || UserChoice+ComputerChoice ==='ss' || UserChoice+ComputerChoice ==='rr'){
+    draw()
+    }
+  }
+  }
 
 
 function main(){
-    const scissors_img=document.getElementById("sissors")
-    scissors_img.addEventListener("click", function(){
-        console.log("clikcME");
+    document.getElementById("s").addEventListener("click", function(){
+        game("s", computerSelection());
     })
 
-    const rock_img=document.getElementById("rock")
-    rock_img.addEventListener("click", function(){
-        console.log("clikcME");
+    document.getElementById("r").addEventListener("click", function(){
+        game("r");
     })
 
-    const paper_img=document.getElementById("paper")
-    paper_img.addEventListener("click", function(){
-        console.log("clikcME");
+     document.getElementById("p").addEventListener("click", function(){
+        game("p");
     })
 }
 
 main();
+
+
+
+
+
+
